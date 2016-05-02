2b093472310bb112d23ad07841eb2ebd925e3c0d
tositeru <uedariki1122@gmail.com>
2016-05-02 16:45:21 +0900

    Part06作成

---
+++ Project/Part06_DrawCall/Main.cpp
@@ -0,0 +1,32 @@
+//*********************************************************
+// The MIT License(MIT)
+// Copyright(c) 2016 tositeru
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files(the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and / or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions :
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//*********************************************************
+
+#include "Scene.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
+{
+	try {
+		Scene sample(1280, 720, L"‚¢‚Ü‚³‚çDirect3D11“ü–å Part3 Buffer‚ÌŽí—Þ");
+		return Win32Application::run(&sample, hInstance, nCmdShow);
+	} catch (std::exception& e) {
+		MessageBoxA(NULL, e.what(), "Error", MB_OK | MB_ICONERROR);
+	}
+	return 1;
+}
